84b6448b0656c682a9ee5f1617392ed3a8d6c7be
frontainer <frontainer@gmail.com>
2020-01-04 22:05:45 +0900

    Updates

---
+++ precache-manifest.d118f57aec74ab4b9cfc7d842c693c97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910d7ea0049dfe98aedbdc4a5c735eeb",
+    "url": "/virtual-shot/index.html"
+  },
+  {
+    "revision": "341e47090fde0a8033f8",
+    "url": "/virtual-shot/static/css/main.c9c90bc8.chunk.css"
+  },
+  {
+    "revision": "72195c52cd4d1629bbeb",
+    "url": "/virtual-shot/static/js/2.81fb1e91.chunk.js"
+  },
+  {
+    "revision": "341e47090fde0a8033f8",
+    "url": "/virtual-shot/static/js/main.cee639b6.chunk.js"
+  },
+  {
+    "revision": "f298ed9bf4c2b81f4dac",
+    "url": "/virtual-shot/static/js/runtime-main.9dc49f1e.js"
+  }
+]);
